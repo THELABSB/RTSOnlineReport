2f8774a5892451a20cb42f296cbcfafd14ccf30c
hungryhansen <hansenlu555@gmail.com>
2022-07-01 12:40:19 -0700

    update

--- RTSApp.py
+++ RTSApp.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 import ReturntoSport
 import athletereport
+import survey
 from PIL import Image
-st.set_page_config(page_title='Return To Sport - Dr. Finer DPT', page_icon=':anger:', layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='Return To Sport - The LAB', page_icon=':anger:', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 
 df=pd.read_csv('returntosport.csv', encoding='UTF-8', header =(0))
@@ -16,7 +17,10 @@ col3.image(logo, width= 200)
 #name=st.selectbox('Athlete Name', df['Name'])
 
 PAGES = {
-    "Athlete Report": athletereport
+    "Athlete Report": athletereport,
+    "Athlete RTS Survey": survey,
+    "Assessment Data": ReturntoSport
+    
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
